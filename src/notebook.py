dc741e711492e649c7c175b2711906d99f1fa625
jeihyde25 <cormyr10@gmail.com>
2025-05-20 16:30:40 +0800

    moved .txt outputs to right folder

--- src/notebook.py
+++ src/notebook.py
@@ -72,7 +72,8 @@ mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
 # Save to txt file
-with open('/tmp/model_metrics.txt', 'w') as f:
+os.makedirs("outputs", exist_ok=True)
+with open('outputs/model_metrics.txt', 'w') as f:
     f.write(f"Mean Absolute Error: {mae}\n")
     f.write(f"Mean Squared Error: {mse}\n")
     f.write(f"R2 Score: {r2}")
